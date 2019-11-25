74a9c49c356972574abd070ad7c8183f1cb474a8
Satoru Nakamura <na.kamura.1263@gmail.com>
2019-11-25 21:24:06 +0900

    minor update

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -4,6 +4,11 @@
 
     <v-app-bar app>
       <v-toolbar-title>Beyond Book Project</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text href="https://github.com/nakamura196/bbp">GitHub</v-btn>
+      </v-toolbar-items>
     </v-app-bar>
   </div>
 </template>
--- app/src/views/Index.vue
+++ app/src/views/Index.vue
@@ -2,101 +2,94 @@
   <div>
     <v-container>
       <v-row>
-        
         <v-col cols="12" sm="9">
-
           <v-row>
             <v-col>
-              <h3>
-          {{((page - 1) * size + 1).toLocaleString()}} - {{(page * size > total ? total : page * size).toLocaleString()}} of {{total.toLocaleString()}} results
-          </h3>
-          </v-col>
-          <v-col>
-
+              <h3>{{((page - 1) * size + 1).toLocaleString()}} - {{(page * size > total ? total : page * size).toLocaleString()}} of {{total.toLocaleString()}} results</h3>
             </v-col>
-            </v-row>
-
-            
+            <v-col></v-col>
+          </v-row>
 
           <template v-if="results.hits">
+            <div class="text-center">
+              <v-pagination
+                v-model="page"
+                :length="pagination_length"
+                :total-visible="7"
+                @input="page_move()"
+              ></v-pagination>
+            </div>
+
+            <v-card class="my-5" v-for="(obj, i) in results.hits.hits" :key="i">
+              <v-row>
+                <v-col cols="12" sm="4">
+                  <v-card-text>
+                    <a target="blank" :href="obj.item.value">
+                      <v-img v-if="obj.image" :src="obj.image.value" class="grey lighten-2"></v-img>
+                    </a>
+                  </v-card-text>
+                </v-col>
+                <v-col cols="12" sm="8">
+                  <v-card-title>
+                    <a target="blank" :href="obj.item.value">{{obj.label.value}}</a>
+                  </v-card-title>
+                  <v-card-text>
+                    <p v-if="obj.description">
+                      <b>Description:</b>
+                      {{obj.description.value}}
+                    </p>
+                    <p v-if="obj.inception">
+                      <b>Inception:</b>
+                      {{obj.inception.value.split("-")[0]}}
+                    </p>
+                    <p v-if="obj.country">
+                      <b>Country:</b>
+                      {{obj.country_label.value}}
+                    </p>
+                    <p v-if="obj.manufacturer">
+                      <b>Manufacturer:</b>
+                      {{obj.manufacturer_label.value}}
+                    </p>
+                  </v-card-text>
+                </v-col>
+              </v-row>
+            </v-card>
 
             <div class="text-center">
-            <v-pagination
-              v-model="page"
-              :length="pagination_length"
-              :total-visible="7"
-              @input="page_move()"
-            ></v-pagination>
-          </div>
-
-          <v-card class="my-5" v-for="(obj, i) in results.hits.hits" :key="i">
-            <v-row>
-              <v-col cols="12" sm="4">
-                <v-card-text>
-                  <v-img v-if="obj.image" :src="obj.image.value" class="grey lighten-2"></v-img>
-                </v-card-text>
-              </v-col>
-              <v-col cols="12" sm="8">
-                <v-card-title>{{obj.label.value}}</v-card-title>
-                <v-card-text>
-                  <p v-if="obj.inception">
-                    <b>Inception:</b>
-                    {{obj.inception.value.split("-")[0]}}
-                  </p>
-                  <p v-if="obj.country">
-                    <b>Country:</b>
-                    {{obj.country_label.value}}
-                  </p>
-                  <p v-if="obj.manufacturer">
-                    <b>Manufacturer:</b>
-                    {{obj.manufacturer_label.value}}
-                  </p>
-                </v-card-text>
-              </v-col>
-            </v-row>
-          </v-card>
-
-          <div class="text-center">
-            <v-pagination
-              v-model="page"
-              :length="pagination_length"
-              :total-visible="7"
-              @input="page_move()"
-            ></v-pagination>
-          </div>
-
+              <v-pagination
+                v-model="page"
+                :length="pagination_length"
+                :total-visible="7"
+                @input="page_move()"
+              ></v-pagination>
+            </div>
           </template>
         </v-col>
         <v-col cols="12" sm="3">
           <template v-if="results.aggregations">
-          <v-card
-            class="my-5"
-            v-for="(obj, index) in results.aggregations"
-            :key="index"
-            
-          >
-            <v-list subheader two-line flat>
-              <v-subheader>{{index}}</v-subheader>
+            <v-card class="my-5" v-for="(obj, index) in results.aggregations" :key="index">
+              <v-list subheader two-line flat>
+                <v-subheader>{{index}}</v-subheader>
 
-              <v-list-item-group multiple>
-                <v-list-item v-for="(bucket, index2) in obj.buckets" :key="index2">
-                  <template v-slot:default="{ active, toggle }">
-                    <v-list-item-action>
-                      <v-checkbox v-model="active" color="primary" @click="toggle"></v-checkbox>
-                    </v-list-item-action>
+                <v-list-item-group multiple>
+                  <v-list-item v-for="(bucket, index2) in obj.buckets" :key="index2">
+                    <template v-slot:default="{ active, toggle }">
+                      <v-list-item-action>
+                        <v-checkbox v-model="active" color="primary" @click="toggle"></v-checkbox>
+                      </v-list-item-action>
 
-                    <v-list-item-content>
-                      <v-list-item-title>{{bucket.key}}</v-list-item-title>
-                    </v-list-item-content>
+                      <v-list-item-content>
+                        <v-list-item-title>{{bucket.key}}</v-list-item-title>
+                      </v-list-item-content>
 
-                    <v-list-item-action>
-                      <v-btn icon>{{bucket.doc_count}}</v-btn>
-                    </v-list-item-action>
-                  </template>
-                </v-list-item>
-              </v-list-item-group>
-            </v-list>
-          </v-card>
+                      <v-list-item-action>
+                        <v-btn icon>{{bucket.doc_count}}</v-btn>
+                      </v-list-item-action>
+                    </template>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-card>
           </template>
         </v-col>
       </v-row>
@@ -112,6 +105,9 @@ export default {
     size: 20
   }),
   mounted() {
+    let param = Object.assign({}, this.$route.query);
+    let class_arr = param.class ? param.class.split(",") : ["Q282"];
+
     class SPARQLQueryDispatcher {
       constructor(endpoint) {
         this.endpoint = endpoint;
@@ -127,23 +123,35 @@ export default {
     }
 
     const endpointUrl = "https://query.wikidata.org/sparql";
-    const sparqlQuery = `#ネコ
-SELECT DISTINCT *
-WHERE 
-{
-  {  ?item wdt:P31 wd:Q15075508;
-        rdfs:label ?label . }
-  union {
-  ?item wdt:P31 wd:Q44;
-        rdfs:label ?label . 
-        }
-        
-  filter(lang(?label) = "en")
-  optional { ?item wdt:P18 ?image }
-  optional { ?item wdt:P571 ?inception }
-   optional { ?item wdt:P495 ?country . ?country rdfs:label ?country_label . filter(lang(?country_label) = "en")}
-  optional { ?item wdt:P176 ?manufacturer . ?manufacturer  rdfs:label ?manufacturer_label . filter(lang(?manufacturer_label) = "en") }
-}`;
+
+    let aaa = "";
+    for (let i = 0; i < class_arr.length; i++) {
+      let class_value = class_arr[i];
+      aaa +=
+        `{  ?item wdt:P31 wd:` +
+        class_value +
+        `;
+        rdfs:label ?label . }`;
+      if (i != class_arr.length - 1) {
+        aaa += " UNION ";
+      }
+    }
+
+    const sparqlQuery =
+      `#ネコ
+      SELECT DISTINCT *
+      WHERE 
+      {
+  ` +
+      aaa +
+      `
+    filter(lang(?label) = "en")
+    optional { ?item schema:description ?description . filter(lang(?description) = "en")}
+    optional { ?item wdt:P18 ?image }
+    optional { ?item wdt:P571 ?inception }
+    optional { ?item wdt:P495 ?country . ?country rdfs:label ?country_label . filter(lang(?country_label) = "en")}
+    optional { ?item wdt:P176 ?manufacturer . ?manufacturer  rdfs:label ?manufacturer_label . filter(lang(?manufacturer_label) = "en") }
+  }`;
 
     const queryDispatcher = new SPARQLQueryDispatcher(endpointUrl);
     queryDispatcher.query(sparqlQuery).then(response => {
@@ -153,23 +161,23 @@ WHERE
 
       //console.log(response);
 
-      let index = {}
-      for(let i = 0; i < results.length; i++){
-        let obj = results[i]
-        for(let key in obj){
-          let value = obj[key].value
+      let index = {};
+      for (let i = 0; i < results.length; i++) {
+        let obj = results[i];
+        for (let key in obj) {
+          let value = obj[key].value;
 
-          if(key == "inception"){
-            value = value.split("-")[0]
+          if (key == "inception") {
+            value = value.split("-")[0];
           }
 
-          if(!index[key]){
-            index[key] = {}
+          if (!index[key]) {
+            index[key] = {};
           }
-          if(!index[key][value]){
-            index[key][value] = 0
+          if (!index[key][value]) {
+            index[key][value] = 0;
           }
-          index[key][value] += 1
+          index[key][value] += 1;
         }
       }
 
@@ -190,8 +198,7 @@ WHERE
         }
       ];
 
-      let query_aggs = {
-      }
+      let query_aggs = {};
 
       for (let i = 0; i < op.length; i++) {
         let obj = op[i];
